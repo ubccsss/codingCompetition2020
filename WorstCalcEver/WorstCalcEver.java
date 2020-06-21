f34c4a038ae9711237876350f59f5968dd94b676
Karan Vasdev <karanvasdev21@gmail.com>
2020-06-20 17:11:56 -0700

    Naming Error

--- WorstCalcEver/WorstCalcEver.java
+++ WorstCalcEver/WorstCalcEver.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class WorstCaseEver
+public class WorstCalcEver
 {
     public static void main(String[] args)
     {
