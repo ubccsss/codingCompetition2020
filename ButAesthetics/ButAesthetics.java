e76921c9288ec1bba62f089e423b74e5bea82363
Karan Vasdev <karanvasdev21@gmail.com>
2020-06-20 17:02:02 -0700

    Finished Questions

---
+++ ButAesthetics/ButAesthetics.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class ButAesthetics
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" ____________________________________________________________");
+        	System.out.println(".  ________________________________________________________  .");
+        	System.out.println(": .                                                          . :");
+        	System.out.println("| |      ______________________________________________      | |");
+        	System.out.println("| |    .:________________________________________________:.    | |");
+        	System.out.println("| |    |                                                  |    | |");
+        	System.out.println("| |    |                                                  |    | |");
+        	System.out.println("| |    |                                                  |    | |");
+        	System.out.println("| |    |                                                  |    | |");
+        	System.out.println("| |    |          TRUST THE NATURAL RECURSION             |    | |");
+        	System.out.println("| |    |                Gregor Kicazles                   |    | |");
+        	System.out.println("| |    |                                                  |    | |");
+        	System.out.println("| |    |                                                  |    | |");
+        	System.out.println("| |    |                                                  |    | |");
+        	System.out.println("| |    |                                                  |    | |");
+        	System.out.println("| |      ______________________________________________      | |");
+        	System.out.println("| |    .:________________________________________________:.    | |");
+        	System.out.println(" ________________________GREGOR INC ____________________________");
+        	System.out.println(".   ________________________________________________________  .");
+        	System.out.println(": .                                                            . :");
+        	System.out.println( "._________________________  [  ]_______________________________.");
+            System.out.println( "._________________________  [  ]_______________________________.");
+            System.out.println( "._________________________ |____|___________________________.");
+
+
+    }
+}
---
+++ DrawTheMarket/DrawTheMarket.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class DrawTheMarket
+{
+    public static void main(String[] args)
+    {
+        System.out.println("       _....._     ");
+        System.out.println("   _.:`.--I--.`:._ ");
+        System.out.println(" .: ..O   I O /,. .");
+        System.out.println("// ..   O I  /  O .lll");
+        System.out.println("//._O.. j I O/ O_..-ll");
+        System.out.println("|| O .-...I/.-. o   ||");
+        System.out.println("||--O--O--I<--O--O--||");
+        System.out.println("|| O .-...I/.-. o   ||");
+        System.out.println(" L._O..  IO/ O_..-.//");
+        System.out.println(" L ..lO  I O /.. //");
+        System.out.println("  L.__... I_____Ol_ ");
+    }
+}
